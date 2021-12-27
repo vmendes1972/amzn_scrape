d800203ca4c4f7c0e06d0a58d5aa566ef64ad075
Victor Mendes <vm1000@vmendes.com>
2021-12-27 00:16:01 +0000

    added new file

---
+++ prj_01/web_scrape_test_02.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup
+import requests
+
+#File = open("out.csv", "a")
+url = "https://www.amazon.co.uk/dp/B07M9ZLDZ6"
+HEADERS = ({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+						'Accept-Language': 'en-US, en;q=0.5'})
+
+webpage = requests.get(url, headers=HEADERS)
+soup = BeautifulSoup(webpage.content, features="lxml")
+
+##############
+#retrieve title
+try:
+    title = soup.find("span", attrs={"id": 'productTitle'})
+    title_value = title.string
+    title_string = title_value.strip().replace(',', '')    		
+except AttributeError:
+	title_string = "NA"
+print("product Title = ", title_string)
+
+##########################
+# retrieving price
+try:
+    price = soup.find("span", attrs={'id': 'priceblock_ourprice'}).string.strip().replace(',', '')
+        # we are omitting unnecessary spaces
+        # and commas form our string
+except AttributeError:
+    price = "NA"
+print("Products price = ", price)
+
+###########################
+# retrieving product rating
+try:
+    rating = soup.find("i", attrs={'class': 'a-icon a-icon-star a-star-4-5'}).string.strip().replace(',', '')
+except AttributeError:
+    try:
+        rating = soup.find("span", attrs={'class': 'a-icon-alt'}).string.strip().replace(',', '')
+    except:
+        rating = "NA"
+print("Overall rating = ", rating)
+
+###########################
+# retrieving customer reviews
+try:
+    review_count = soup.find("span", attrs={'id': 'acrCustomerReviewText'}).string.strip().replace(',', '')
+except AttributeError:
+    review_count = "NA"
+print("Total reviews = ", review_count)
+ 
+#############################
+# print availablility status
+try:
+    available = soup.find("div", attrs={'id': 'availability'})
+    available = available.find("span").string.strip().replace(',', '')
+except AttributeError:
+    available = "NA"
+print("Availability = ", available)
+
+###################
+# print sold by
+try:
+    soldby = soup.find("span", attrs={'id': 'sellerProfileTriggerId'}).string.strip().replace(',', '')
+except AttributeError:
+    soldby = "NA"
+print("Sold By = ", soldby)
